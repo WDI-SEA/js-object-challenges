4a4da208b3c17b06558da688ec34f621f53da122
Pinyi Yang <pyyanguw@gmail.com>
2019-05-29 15:23:35 -0700

    Answer for object challenge

---
+++ js/pizza.js
@@ -0,0 +1,31 @@
+//1. create menu object
+var menu = {
+    Supreme: {
+        topping:['pepperoni', 'sausage', 'onion', 'olive'],
+        price: 21
+    },
+    Hawaiian: {
+        topping: ['ham', 'pineapple', 'onion'],
+        price: 18
+    },
+    Veggi: {
+        topping:['pepper', 'onion', 'mushroom'],
+        price: 16
+    }
+}
+
+//2. Print the name of all Pizzas
+console.log("Names of pizzas are " + Object.keys(menu).join(", ") +".");
+
+//3. check banana pizza
+if (menu.hasOwnProperty('banana')) {
+    console.log("It has banana pizza.");
+} else {
+    console.log("It doesn't has banana pizza.");
+}
+
+//Bonus
+for (var pizza in menu) {
+    console.log(pizza + "   $" + menu[pizza].price);
+    console.log(menu[pizza].topping.join(", ")+ '\n');
+}
