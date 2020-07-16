d69b377860828102c941ee63e90c9f1defcb9d5f
margaret-jihua <margaret.jihua@gmail.com>
2020-07-15 17:08:02 -0700

    finish question 1 & 2

---
+++ js/pizza.js
@@ -0,0 +1,16 @@
+const menu = {
+    cheese:{
+        toppings: ["cheese", "suassage", "tomato"],
+        price: 5
+    },
+    peperoni:{
+        topping: ["bascon", "cheese","peperoni"],
+        price: 7
+    },
+    Hawaiian:{
+        toppings: ["pineapple", "ham", "chiken"],
+        price: 6
+    }
+}
+
+console.log(Object.keys(menu));
